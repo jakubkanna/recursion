7aad0811c773bf4b2b3beea9987d2bf1c01d9b84
Jakub Kanna <hello.jakubkanna@gmail.com>
2023-12-14 17:26:45 +0100

    added merge sort fn

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
 #!/usr/bin/env node
 
-// Assignment 1
-
-/**
- * The Fibonacci sequence is a sequence in which
- * each number is the sum of the two preceding ones.
- *  The sequence commonly starts from 0 and 1.
- */
+// Assignment 1 : fibonacci series
 
 let array = [];
 
@@ -17,24 +11,50 @@ function fibs(n) {
   return array;
 }
 
+// console.log(fibs(8));
+
 function fibsRec(i) {
-  //get fib n for i
+  // get fib n for i
   const fibN = (n) => (n <= 1 ? n : fibN(n - 1) + fibN(n - 2));
 
-  //insert fibn for array[n]
+  // new array item
   array[i] = fibN(i);
 
-  //loop until it reaches 0
-  if (i === 0) return array;
-  return fibsRec(i - 1);
+  // loop
+  return i === 0 ? array : fibsRec(i - 1);
 }
 
-// console.log(fibs(8));
-// console.log(fibsRec(8));
+console.log("fibonacci series:", fibsRec(8));
 
-// Assignment 1
+// Assignment 2 : mergeSort
 
-/**
- * Build a function mergeSort that takes in an array and returns a sorted array,
- * using a recursive merge sort methodology.
- */
+let someArray = [5, 2, 1, 3, 6, 4];
+
+function mergeSort(array) {
+  // Base case
+  if (array.length === 1) return array;
+
+  //get rounded midpoint in case array is odd
+  const mid = Math.floor(array.length / 2);
+
+  //split array at mid
+  const left = mergeSort(array.slice(0, mid));
+  const right = mergeSort(array.slice(mid));
+
+  //merge
+  return merge(left, right);
+}
+function merge(left, right) {
+  let result = [];
+
+  //loop through arrays until one is empty
+  while (left.length && right.length) {
+    //compare and push smaller item to result
+    left[0] < right[0] ? result.push(left.shift()) : result.push(right.shift());
+  }
+
+  //in case that there are items left in left or right include them also
+  return [...result, ...left, ...right];
+}
+
+console.log("Sorted array:", mergeSort(someArray));
