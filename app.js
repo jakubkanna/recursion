ff56d1874b0a70e63b3c821153add047f6297538
Jakub Kanna <hello.jakubkanna@gmail.com>
2023-12-14 17:29:44 +0100

    cleanup

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-// Assignment 1 : fibonacci series
+// Assignment 1: Fibonacci series
 
 let array = [];
 
@@ -14,19 +14,19 @@ function fibs(n) {
 // console.log(fibs(8));
 
 function fibsRec(i) {
-  // get fib n for i
+  // Get Fibonacci number for i
   const fibN = (n) => (n <= 1 ? n : fibN(n - 1) + fibN(n - 2));
 
-  // new array item
+  // Add new Fibonacci number to the array
   array[i] = fibN(i);
 
-  // loop
+  // Recursion
   return i === 0 ? array : fibsRec(i - 1);
 }
 
-console.log("fibonacci series:", fibsRec(8));
+console.log("Fibonacci series:", fibsRec(8));
 
-// Assignment 2 : mergeSort
+// Assignment 2: Merge Sort
 
 let someArray = [5, 2, 1, 3, 6, 4];
 
@@ -34,26 +34,27 @@ function mergeSort(array) {
   // Base case
   if (array.length === 1) return array;
 
-  //get rounded midpoint in case array is odd
+  // Get rounded midpoint in case the array length is odd
   const mid = Math.floor(array.length / 2);
 
-  //split array at mid
+  // Split array at the midpoint and run mergeSort for it
   const left = mergeSort(array.slice(0, mid));
   const right = mergeSort(array.slice(mid));
 
-  //merge
+  // Merge
   return merge(left, right);
 }
+
 function merge(left, right) {
   let result = [];
 
-  //loop through arrays until one is empty
+  // Loop through arrays until one is empty
   while (left.length && right.length) {
-    //compare and push smaller item to result
+    // Compare and push the smaller item to the result
     left[0] < right[0] ? result.push(left.shift()) : result.push(right.shift());
   }
 
-  //in case that there are items left in left or right include them also
+  // Include any remaining items from left or right
   return [...result, ...left, ...right];
 }
 
